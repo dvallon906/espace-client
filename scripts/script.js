c1019f8bfcbd78c55bc9a7a73f3a82d1506519fd
dvallon906 <d.vallon@icloud.com>
2024-05-20 09:40:57 +0200

    Ajout EventClick

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 
+
+
+const btnFrequentation = document.querySelector("#btnFrequentation");
+btnFrequentation.addEventListener("click", (event) => {
+    var link = "https://www.nonstopgym.com/frequentation/";
+    open(link,"_blank")
+});
+
+const rowVotreContrat = document.querySelector("#rowVotreContrat");
+rowVotreContrat.addEventListener("click", (event) => {
+    alert("Détail du contrat du client")
+});
+
